342f1e1742f713739aeb6bc5ead4b1be58d56102
mhtf19 <mhtf19@gmail.com>
2020-10-21 13:47:40 -0400

    cloud stuff

--- Assets/Scenes/FollowCam.cs
+++ Assets/Scenes/FollowCam.cs
@@ -6,6 +6,10 @@ public class FollowCam : MonoBehaviour
 {
     static public GameObject POI;
 
+    [Header("Set in Inspector")]
+    public float easing = .05f;
+    public Vector2 minXY = Vector2.zero;
+
     [Header("Set Dynamically")]
     public float camZ;
 
@@ -24,8 +28,16 @@ public class FollowCam : MonoBehaviour
     {
         if(POI == null) return;
         Vector3 destination = POI.transform.position;
+
+        //limit x and y to min
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+
+        //interpolate from current camera pos towards destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
 
         transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
     }
 }
