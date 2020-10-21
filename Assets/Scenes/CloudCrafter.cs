8fce4956dc6514ce3dc88d8cde983712e81d9708
mhtf19 <mhtf19@gmail.com>
2020-10-21 15:55:09 -0400

    started cloud random generator

---
+++ Assets/Scenes/CloudCrafter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCrafter : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public int numClouds = 40;
+    public GameObject cloudPrefab;
+    public Vector3 cloudPosMin = new Vector3(-50, -5, 10);
+    public Vector3 cloudPosMax = new Vector3(150, 100, 10);
+    public float cloudScaleMin = 1;
+    public float cloudScaleMax = 3;
+    public float cloudSpeedMult = .5f;
+    private GameObject[] cloudInstances;
+
+    void Awake(){
+        cloudInstances = new GameObject[numClouds];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
