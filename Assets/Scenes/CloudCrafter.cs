3cf7e08d0f5f618aaf46f9d53f0fbe3737d99080
mhtf19 <mhtf19@gmail.com>
2020-10-22 00:06:52 -0400

    Cloud movement

--- Assets/Scenes/CloudCrafter.cs
+++ Assets/Scenes/CloudCrafter.cs
@@ -16,6 +16,28 @@ public class CloudCrafter : MonoBehaviour
 
     void Awake(){
         cloudInstances = new GameObject[numClouds];
+        GameObject anchor = GameObject.Find("CloudAnchor");
+        GameObject cloud;
+        for(int i = 0; i<numClouds; i++){
+            cloud = Instantiate<GameObject>(cloudPrefab);
+            Vector3 cPos = Vector3.zero;
+            cPos.x = Random.Range(cloudPosMin.x, cloudPosMax.x);
+            cPos.y = Random.Range(cloudPosMin.y, cloudPosMax.y);
+
+            float scaleU = Random.value;
+            float scaleVal = Mathf.Lerp(cloudScaleMin, cloudScaleMax, scaleU);
+
+            cPos.y = Mathf.Lerp(cloudPosMin.y, cPos.y, scaleU);
+
+            cPos.z = 100-90*scaleU;
+
+            cloud.transform.position = cPos;
+            cloud.transform.localScale = Vector3.one*scaleVal;
+
+            cloud.transform.SetParent(anchor.transform);
+
+            cloudInstances[i] = cloud;
+        }
     }
 
     // Start is called before the first frame update
@@ -27,6 +49,20 @@ public class CloudCrafter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (GameObject cloud in cloudInstances)
+        {
+            //get cloud scale/pos
+            float scaleVal = cloud.transform.localScale.x;
+            Vector3 cPos = cloud.transform.position;
+
+            //move bigger clouds faster
+            cPos.x -=scaleVal*Time.deltaTime*cloudSpeedMult;
+
+            //move a cloud back to start
+            if(cPos.x <= cloudPosMin.x){
+                cPos.x = cloudPosMax.x;
+            }
+            cloud.transform.position = cPos;
+        }
     }
 }
