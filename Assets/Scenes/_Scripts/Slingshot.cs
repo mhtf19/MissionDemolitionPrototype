5e6a3c54f4bf956ad58f61a5a21b3641125780ef
mhtf19 <mhtf19@gmail.com>
2020-10-22 12:08:57 -0400

    projectile_line code and attachment to object

--- Assets/Scenes/_Scripts/Slingshot.cs
+++ Assets/Scenes/_Scripts/Slingshot.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    static private Slingshot S;
     //fields set in Unity Inspector pane
     [Header("Set in Inspector")]
     public GameObject prefabProjectile;
@@ -16,8 +17,16 @@ public class Slingshot : MonoBehaviour
     public GameObject projectile;
     public bool aimingMode;
     private Rigidbody projectileRigidbody;
+
+    static public Vector3 LAUNCH_POS {
+        get{
+            if(S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
     
     void Awake(){
+        S = this;
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
